fe1f858dd31a9040b79a5ee68d0721169a80043e
LucaGoubelle <luca.goubelle97@gmail.com>
2025-08-08 19:25:09 +0200

    Add deps in constructor (white side bar handler)

--- solver5x5/handlers/centers/whites/white_side_bar_handler.py
+++ solver5x5/handlers/centers/whites/white_side_bar_handler.py
@@ -1,8 +1,14 @@
 from pyrubik.data.cube import Cube
+from pyrubik.moves.mover import Mover
+from solver_helpers.seekers.center_5_seeker import Center5Seeker
 
 
 class WhiteSideBarHandler:
     """ white side bars handler """
+    
+    def __init__(self):
+        self.center_seeker = Center5Seeker()
+        self.mover = Mover()
 
     def handle(self, cube: Cube) -> Cube:
         cube = self._do_first_side_bar(cube)
@@ -21,8 +27,12 @@ class WhiteSideBarHandler:
         return cube
 
     def _find_center_edge1(self, cube: Cube) -> Cube:
-        #todo: implement this
-        return cube
+        focus_center_edge: str = cube.front[2][3]
+        if focus_center_edge == "white":
+            return cube # nothing to do
+        else:
+            # todo: implement this part
+            return cube
     
     def _solve_first_center_corner1(self, cube: Cube) -> Cube:
         #todo: implement this
@@ -38,9 +48,19 @@ class WhiteSideBarHandler:
 
     def _do_second_side_bar(self, cube: Cube) -> Cube:
         cube = self._find_center_edge2(cube)
-        #todo: implement this
+        cube = self._solve_first_center_corner2(cube)
+        cube = self._solve_second_center_corner2(cube)
+        #todo: implement remaining lines
         return cube
     
     def _find_center_edge2(self, cube: Cube) -> Cube:
+        #todo: implement this
+        return cube
+
+    def _solve_first_center_corner2(self, cube: Cube) -> Cube:
+        #todo: implement this
+        return cube
+    
+    def _solve_second_center_corner2(self, cube: Cube) -> Cube:
         #todo: implement this
         return cube
